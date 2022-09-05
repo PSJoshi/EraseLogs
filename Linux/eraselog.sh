ecbf9331c6fb72e615f6e059d9fc5e24374f11dd
Pradyumna Joshi <joshi.pradyumna@gmail.com>
2022-09-05 12:30:22 +0530

    Create eraselog.sh

---
+++ Linux/eraselog.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+clearlog(){
+    export HISTFILE=
+    unset HISTFILE
+    shred -zu ~/.bash_history
+    find /home/ -name ".bash_history" -exec shred -zu {} \; -exec touch {} \;
+    history -c
+    touch ~/.bash_history
+    unset HISTFILE HISTSIZE
+    set history=0
+    set +o history
+    unset HISTFILE
+    find / -type f -exec {}
+    echo "Logs cleared!"
+    sleep 1.5
+}
+
+function isRoot () {
+    if [ "$EUID" -ne 0 ]; then
+        return 1
+    fi
+}
+
+function enableHistory () {
+        if [[ -L ~/.bash_history ]]; then
+                rm -rf ~/.bash_history
+                echo "" > ~/.bash_history
+                echo "[+] Disabled sending history to /dev/null"
+        fi
+
+        if [[ -L ~/.zsh_history ]]; then
+                rm -rf ~/.zsh_history
+                echo "" > ~/.zsh_history
+                echo "[+] Disabled sending zsh history to /dev/null"
+        fi
+
+        export HISTFILESIZE=""
+        export HISTSIZE=50000
+        echo "[+] Restore HISTFILESIZE & HISTSIZE default values."
+
+        set -o history
+        echo "[+] Enabled history library"
+
+        echo
+        echo "Permenently enabled bash log."
+}
+
+function disableHistory () {
+        ln /dev/null ~/.bash_history -sf
+        echo "[+] Permanently sending bash_history to /dev/null"
+
+        if [ -f ~/.zsh_history ]; then
+                ln /dev/null ~/.zsh_history -sf
+                echo "[+] Permanently sending zsh_history to /dev/null"
+        fi
+
+        export HISTFILESIZE=0
+        export HISTSIZE=0
+        echo "[+] Set HISTFILESIZE & HISTSIZE to 0"
+
+        set +o history
+        echo "[+] Disabled history library"
+
+        echo
+        echo "Permenently disabled bash log."
+}
