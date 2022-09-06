61b4a1de704930935165f2ebf8e591847a3b5b7a
Pradyumna Joshi <joshi.pradyumna@gmail.com>
2022-09-06 10:51:26 +0530

    Update eraselog.sh

--- Linux/eraselog.sh
+++ Linux/eraselog.sh
@@ -66,23 +66,82 @@ function disable_history () {
         echo "Permenently disabled bash log."
 }
 
-LOGS_FILES=(
-        /var/log/messages # General message and system related stuff
-        /var/log/auth.log # Authenication logs
-        /var/log/kern.log # Kernel logs
-        /var/log/cron.log # Crond logs
-        /var/log/maillog # Mail server logs
-        /var/log/boot.log # System boot log
-        /var/log/mysqld.log # MySQL database server log file
-        /var/log/qmail # Qmail log directory
-        /var/log/httpd # Apache access and error logs directory
-        /var/log/lighttpd # Lighttpd access and error logs directory
-        /var/log/secure # Authentication log
-        /var/log/utmp # Login records file
-        /var/log/wtmp # Login records file
-        /var/log/yum.log # Yum command log file
-        /var/log/system.log # System Log
-)
+  logs = [
+            "/var/log/messages", # General message and system related stuff
+            "/var/log/auth.log", # Authenication logs
+            "/var/log/kern.log", # Kernel logs
+            "/var/log/cron.log", # Crond logs
+            "/var/log/maillog", # Mail server logs
+            "/var/log/boot.log", # System boot log
+            "/var/log/mysqld.log", # MySQL database server log file
+            "/var/log/qmail", # Qmail log directory
+            "/var/log/httpd", # Apache access and error logs directory
+            "/var/log/lighttpd", # Lighttpd access and error logs directory
+            "/var/log/secure", # Authentication log
+            "/var/log/utmp", # Login records file
+            "/var/log/wtmp", # Login records file
+            "/var/log/yum.log", # Yum command log file
+            "/var/log/system.log", # System Log
+            "/var/log/lastlog", 
+            "/var/log/warn", 
+            "/var/log/poplog", 
+            "/var/log/qmail", 
+            "/var/log/smtpd", 
+            "/var/log/telnetd", 
+            "/var/log/auth", 
+            "/var/log/auth.log", 
+            "/var/log/cups/access_log", 
+            "/var/log/cups/error_log", 
+            "/var/log/thttpd_log", 
+            "/var/log/spooler", 
+            "/var/spool/tmp", 
+            "/var/spool/errors", 
+            "/var/spool/locks", 
+            "/var/log/nctfpd.errs", 
+            "/var/log/acct", 
+            "/var/apache/log", 
+            "/var/apache/logs", 
+            "/usr/local/apache/log", 
+            "/usr/local/apache/logs", 
+            "/usr/local/www/logs/thttpd_log", 
+            "/var/log/xferlog", 
+            "/var/log/proftpd/xferlog.legacy", 
+            "/var/log/proftpd.xferlog", 
+            "/var/log/proftpd.access_log", 
+            "/var/log/httpd/error_log", 
+            "/var/log/httpsd/ssl_log", 
+            "/var/log/httpsd/ssl.access_log", 
+            "/var/adm", 
+            "/var/run/utmp", 
+            "/etc/wtmp", 
+            "/etc/utmp", 
+            "/etc/mail/access", 
+            "/var/log/mail/info.log", 
+            "/var/log/mail/errors.log", 
+            "/var/log/httpd/*_log", 
+            "/var/log/ncftpd/misclog.txt", 
+            "/var/account/pacct", 
+            "/var/log/snort", 
+            "/var/log/bandwidth", 
+            "/var/log/explanations", 
+            "/var/log/syslog", 
+            "/var/log/user.log", 
+            "/var/log/daemons/info.log", 
+            "/var/log/daemons/warnings.log", 
+            "/var/log/daemons/errors.log", 
+            "/etc/httpd/logs/error_log", 
+            "/etc/httpd/logs/*_log", 
+            "/var/log/mysqld/mysqld.log"
+            "/root/.ksh_history", 
+            "/root/.bash_history", 
+            "/root/.sh_history", 
+            "/root/.history", 
+            "/root/*_history", 
+            "/root/.login", 
+            "/root/.logout", 
+            "/root/.bash_logut", 
+            "/root/.Xauthority"
+	]
 
 function clearLogs () {
         for i in "${LOGS_FILES[@]}"
